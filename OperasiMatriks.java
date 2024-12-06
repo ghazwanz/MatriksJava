4050dcc7d7e15fb73a1cfa297739c38a8793cc7b
Ghazwan Ababil <ababilghazwan@gmail.com>
2024-12-06 09:59:49 +0700

    initial

---
+++ OperasiMatriks.java
@@ -0,0 +1,39 @@
+package Matriks;
+
+import java.util.Scanner;
+
+public class OperasiMatriks {
+    static Scanner sc = new Scanner(System.in); 
+    public static void main(String[] args) {
+        int menu;
+        System.out.println("===Daftar Pilihan Matriks===");
+        System.out.println("1. Operasi Tambah dan Kurang Matriks");
+        System.out.println("2. Operasi Perkalian Matriks");
+        System.out.println("3. Invers Matriks");
+        System.out.println("4. Transpose Matriks");
+        System.out.println("5. Keluar");
+        System.out.print("Pilih Menu:");
+        menu = sc.nextInt();
+        switch (menu) {
+            case 1:
+                
+                break;
+            case 2:
+                
+                break;
+            case 3:
+                
+                break;
+            case 4:
+                
+                break;
+            case 5:
+                
+                break;
+        
+            default:
+                System.out.println("Input menu tidak valid. Coba lagi.");
+                break;
+        }
+    }
+}
